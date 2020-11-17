3a0e5ed438e57d571ebaeea523842c04bc881924
Victor <victorzevallos@protonmail.com>
2020-11-17 15:25:17 -0500

    Update user resources with processErrors

--- api/resources/users/users.routes.js
+++ api/resources/users/users.routes.js
@@ -6,8 +6,18 @@ const log = require('../../../utils/logger')
 const { validateUser, validateLogin } = require('./users.validate')
 const config = require('../../../config')
 const userRepository = require('./user.repository')
+const processErrors = require('../../libs/errorHandler').processErrors
 
 const usersRouter = express.Router()
+
+class UserExistsError extends Error {
+  constructor(message) {
+    super(message)
+    this.message = message || 'El email o usuario ya están asociados con una cuenta.'
+    this.status = 409
+    this.name = "UsuarioExisteError"
+  }
+}
 
 function bodyToLowerCase(req, res, next) {
   req.body.username && (req.body.username = req.body.username.toLowerCase())
@@ -15,76 +25,42 @@ function bodyToLowerCase(req, res, next) {
   next()
 }
 
-usersRouter.get('/', (req, res) => {
-  userRepository.getUsers()
+usersRouter.get('/', processErrors((req, res) => {
+  return userRepository.getUsers()
     .then(users => {
       res.json(users)
     })
-    .catch(err => {
-      log.error('Error al obtener todos los usuarios', err)
-      res.sendStatus(500)
-    })
-})
+}))
 
-usersRouter.post('/', [validateUser, bodyToLowerCase], (req, res) => {
+usersRouter.post('/', [validateUser, bodyToLowerCase], processErrors((req, res) => {
   const user = req.body
-
-  userRepository.userExists(user.username, user.email)
+  return userRepository.userExists(user.username, user.email)
     .then(userExists => {
       if (userExists) {
         log.warn('Email o username ya existe en la base de datos')
-        return res.status(409).send('El email o username ya están asociados a una cuenta')
+        throw new UserExistsError()
       }
 
-      bcrypt.hash(user.password, 10, (err, hashedPassword) => {
-        if (err) {
-          log.error('Ocurrió un error al tratar de obtener el hash de una contraseña', err)
-          return res.status(500).send("Ocurrió un error procesando creación del usuario")
-        }
-
-        userRepository.createUser(user, hashedPassword)
-          .then(user => {
-            log.info("Usuario creado", { ...user, password: hashedPassword })
-            res.status(201).send("Usuario creado exitósamente")
-          })
-          .catch(err => {
-            log.error('Ocurrió un error al tratar de crear un nuevo usuario', user)
-            res.status(500).send('Ocurrio un error al tratar de crear un nuevo usuario.')
-          })
-
-      })
+      return bcrypt.hash(user.password, 10)
     })
-    .catch(err => {
-      log.error(`Ocurrió un error al tratar de verificar si ya existe un usuario
-        [${user.usernmae}] con email [${user.email}]`)
-      res.status(500).send('Ocurrió un error al tratar de crear un nuevo usuario.')
+    .then(hashedPassword => {
+      return userRepository.createUser(user, hashedPassword)
+        .then(user => {
+          log.info("Usuario creado", { ...user, password: hashedPassword })
+          res.status(201).send("Usuario creado exitósamente")
+        })
     })
-})
+}))
 
-usersRouter.post('/login', [validateLogin, bodyToLowerCase], async (req, res) => {
+usersRouter.post('/login', [validateLogin, bodyToLowerCase], processErrors(async (req, res) => {
   const user = req.body
-  let registeredUser
 
-  try {
-    registeredUser = await userRepository.getUser({ username: user.username })
-  } catch(err) {
-    log.error(`Ocurrió un error al tratar de determinar si el usuario [${user.username}] ya existe`, err)
-    return res.status(500).send('Ocurrió un error durante el proceso de login.')
-  }
-
+  const registeredUser = await userRepository.getUser({ username: user.username })
   if (!registeredUser) {
     log.info(`Usuario ${user.username} no pudo ser autenticado`)
     return res.status(400).send('Credenciales incorrectas. Asegúrate que el username y contraseña sean correctas.')
   }
-
-  let correctPassword
-
-  try {
-    correctPassword = await bcrypt.compare(user.password, registeredUser.password)
-  } catch (err) {
-    log.error(`Ocurrió un error al tratar de verificar si la contraseña es correcta`, err)
-    return res.status(500).send('Ocurrió un error durante el proceso de login.')
-  }
+  const correctPassword = await bcrypt.compare(user.password, registeredUser.password)
 
   if (correctPassword) {
     const token = jwt.sign({ id: registeredUser.id }, config.jwt.secret, { expiresIn: config.jwt.expiryTime })
@@ -94,6 +70,6 @@ usersRouter.post('/login', [validateLogin, bodyToLowerCase], async (req, res) =>
     log.info(`Usuario ${user.username} no completó autenticación. Contraseña incorrecta`)
     res.status(400).send('Credenciales incorrectas. Asegúrate que el username y contraseña sean correctas')
   }
-})
+}))
 
 module.exports = usersRouter
